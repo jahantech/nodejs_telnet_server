b819ad54cb08effcbabff4ce5a3c700dfa3ed687
jahantech <jahansyed@hotmail.co.uk>
2016-07-22 19:19:30 +0100

    Client end session

--- server.js
+++ server.js
@@ -12,24 +12,25 @@ var DisconnectedUserList = []
 telnetServer.on('connection',function(client){
 	client.name = client.remoteAddress+":"+client.remotePort;
 	ActiveUserList.push(client)
-	broadcast(client.name+" has joined the conversation\r\n",client)
+	broadcast(" has joined the conversation\r\n",client)
 	client.on('data',function(data){
 		broadcast(data,client);
+	})
+
+	client.on('end', function(data){
+		broadcast("has left the conversation",client);
 	})
 })
 
 function broadcast(message,client){
 	var i; 
 	for(var i=0;i<ActiveUserList.length;i++){
-		console.log(ActiveUserList[i].name);
 		if (client !== ActiveUserList[i]){
 			console.log(util.inspect(ActiveUserList[i], false, null));
 			if (ActiveUserList[i].writable === true){
-				console.log("I am here");
 				ActiveUserList[i].write(client.name+":"+message);
 			}
 			else{
-				console.log("Can't write to socket");
 				DisconnectedUserList.push(ActiveUserList[i]);
 				ActiveUserList[i].destroy();
 			}
